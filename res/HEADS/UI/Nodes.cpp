8b937a3f1e6cb607c8932153e95095c737c30bd4
XdzzzzzzzYQ <xiaodouzizyq@gmail.com>
2022-10-24 22:47:09 +0800

    10.24 Node structure update

--- res/HEADS/UI/Nodes.cpp
+++ res/HEADS/UI/Nodes.cpp
@@ -1,5 +1,33 @@
 #include "Nodes.h"
 
+unsigned int Nodes::id_buff = 1;
+
+std::vector<ImU32> Nodes::n_color_list = { IM_COL32(0,246,83,255), IM_COL32(246,215,0,255) };
+std::vector<ImU32> Nodes::pin_color_list = {};
+
+std::unordered_map<Parameters*, Nodes::ParaLink> Nodes::n_in_link = {};
+std::unordered_map<Parameters*, Nodes::ParaLink> Nodes::n_out_link = {};
+
+void Nodes::PushIn(const Parameters& _in)
+{
+	n_in.push_back(_in);
+}
+
+void Nodes::PushOut(const Parameters& _out)
+{
+	n_out.push_back(_out);
+}
+
+void Nodes::LinkIn(int _self_idx, Nodes* _tar, int _idx)
+{
+	n_in_link[&n_in[_self_idx]] = std::make_pair(_tar, _idx);
+}
+
+void Nodes::LinkOut(int _self_idx, Nodes* _tar, int _idx)
+{
+	n_out_link[&n_out[_self_idx]] = std::make_pair(_tar, _idx);
+}
+
 Nodes::Nodes()
 	:Nodes(NONE_NODE)
 {
@@ -7,13 +35,14 @@ Nodes::Nodes()
 }
 
 Nodes::Nodes(NodeType _type)
-	:Nodes("[Unknown]", _type)
+	: Nodes("[Unknown]", _type)
 {
 
 }
 
 Nodes::Nodes(const char* _name, NodeType _type)
-	:n_name(_name), n_type(_type)
+	: n_name(_name), n_type(_type)
 {
-
+	n_id = id_buff;
+	id_buff++;
 }
--- res/HEADS/UI/Nodes.h
+++ res/HEADS/UI/Nodes.h
@@ -13,22 +13,39 @@ enum NodeType
 
 class Nodes : public Transform2D
 {
+public:
 	//			|	Nodes   |   Index=-1   |
 	typedef std::pair<Nodes*, int> ParaLink;
-
+private:
+	static unsigned int id_buff;
 public:
+	static std::vector<ImU32> n_color_list;
+	static std::vector<ImU32> pin_color_list;
+public:
+	bool is_open{ true };
+	bool is_select{ false };
+	bool is_activated{ false };
+
+	unsigned int n_id;
+
 	NodeType n_type;
 	std::string n_name;
 	std::string n_label;
 
-
-	ImU32 n_color;
+	ImU32 n_color;    //ONLY for custom nodes
 public:
 	std::vector<Parameters> n_in;
 	std::vector<Parameters> n_out;
 
-	std::vector<ParaLink> n_in_link;     // ->[in   out]
-	std::vector<ParaLink> n_out_link;    //   [in   out]->
+	static std::unordered_map<Parameters*, ParaLink> n_in_link;     // ->[in   out]			  //Store globle Data
+	static std::unordered_map<Parameters*, ParaLink> n_out_link;    //   [in   out]->		  //Store globle Data
+	static Parameters* GetParamPtr(Nodes* _tar,int _idx, bool _type) { return _type ? &_tar->n_in[_idx] : &_tar->n_out[_idx]; }
+	static Parameters* GetParamPtr(const ParaLink& _link, bool _type) { return GetParamPtr(_link.first, _link.second, _type); }
+
+	void PushIn (const Parameters& _in);
+	void PushOut(const Parameters& _out);
+	void LinkIn (int _self_idx, Nodes* _tar, int _idx);
+	void LinkOut(int _self_idx, Nodes* _tar, int _idx);
 public:
 	Nodes();
 	Nodes(NodeType _type);
