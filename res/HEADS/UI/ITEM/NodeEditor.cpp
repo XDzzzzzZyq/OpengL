eebc7f12f197598c32367b0d0e368f0dd5a84b82
XdzzzzzzzYQ <xiaodouzizyq@gmail.com>
2022-10-24 22:48:08 +0800

    10.24 basic node rendering

--- res/HEADS/UI/ITEM/NodeEditor.cpp
+++ res/HEADS/UI/ITEM/NodeEditor.cpp
@@ -1,32 +1,59 @@
 #include "NodeEditor.h"
 
+const ImVec2 NodeTrans::GetInPinPos(const ImVec2& _header_size, float _offset, int _idx)
+{
+	ImVec2 inp_curs = min + _header_size;
+	inp_curs.y += _offset * _idx;
+	return inp_curs;
+}
+
+const ImVec2 NodeTrans::GetOutPinPos(const ImVec2& _header_size, float _offset, int _idx)
+{
+	ImVec2 outp_curs = ImVec2(max.x, min.y) + _header_size;
+	outp_curs.y += _offset * _idx;
+	return outp_curs;
+}
+
+
+
 NodeEditor::NodeEditor()
 	:NodeEditor(NONE_NODE_EDITOR)
 {
 }
 
 NodeEditor::NodeEditor(NodeEditorType type)
-	:_type(type)
+	: _type(type)
 {
-	EventList[GenIntEvent(1, 0, 0, 3, 0)] = std::bind(&NodeEditor::SHIFT_MMB		, this);
-	EventList[GenIntEvent(2, 0, 0, 3, 0)] = std::bind(&NodeEditor::CTRL_MMB			, this);
-	EventList[GenIntEvent(0, 0, 0, 0, 1)] = std::bind(&NodeEditor::SCRLL			, this);
-	EventList[GenIntEvent(0, 0, 0, 0,-1)] = std::bind(&NodeEditor::SCRLL			, this);
-	EventList[GenIntEvent(1, 0, 0, 0, 1)] = std::bind(&NodeEditor::SCRLL			, this);
-	EventList[GenIntEvent(1, 0, 0, 0,-1)] = std::bind(&NodeEditor::SCRLL			, this);
-	
-	Nodes add{ "Add", SCL_MATH_NODE };
-	_node_pool.emplace_back(add);
+	EventList[GenIntEvent(0, 0, 0, 1, 0)] = std::bind(&NodeEditor::LMB, this);
+	EventList[GenIntEvent(0, 0, 0, 3, 0)] = std::bind(&NodeEditor::SHIFT_MMB, this);
+	EventList[GenIntEvent(1, 0, 0, 3, 0)] = std::bind(&NodeEditor::SHIFT_MMB, this);
+	EventList[GenIntEvent(2, 0, 0, 3, 0)] = std::bind(&NodeEditor::CTRL_MMB, this);
+	EventList[GenIntEvent(0, 0, 0, 0, 1)] = std::bind(&NodeEditor::SCRLL, this);
+	EventList[GenIntEvent(0, 0, 0, 0, -1)] = std::bind(&NodeEditor::SCRLL, this);
+	EventList[GenIntEvent(1, 0, 0, 0, 1)] = std::bind(&NodeEditor::SCRLL, this);
+	EventList[GenIntEvent(1, 0, 0, 0, -1)] = std::bind(&NodeEditor::SCRLL, this);
 
-	Nodes sub{ "Subtract", SCL_MATH_NODE };
-	sub.SetPos({ 50,50 });
-	_node_pool.emplace_back(sub);
+	Nodes* add = new Nodes{ "Add", SCL_MATH_NODE };
+	add->PushIn({ INT_PARA, "int1" });
+	add->PushIn({ INT_PARA, "int2" });
+	add->PushIn({ FLOAT_PARA, "float_test" });
+	add->PushOut({ INT_PARA, "int3" });
+	add->PushOut({ FLOAT_PARA, "float2" });
+	PushNode(add);
+
+	Nodes* sub = new Nodes{ "Subtract", VEC_MATH_NODE };
+	sub->SetPos({ 100,50 });
+	sub->PushOut({ FLOAT_PARA, "outp" });
+	sub->PushOut({ FLOAT_PARA, "outp2" });
+	add->LinkIn(0, sub, 0);
+	add->LinkIn(2, sub, 1);
+	PushNode(sub);
 }
 
 void NodeEditor::Render(const char* _lable, const ImVec2& _size /*= {0,0}*/)
 {
 	ImGui::Separator();
-	if (ImGui::BeginChild(_lable, ImGui::GetContentRegionAvail())) {
+	if (ImGui::BeginChild(_lable, ImGui::GetContentRegionAvail(), false, ImGuiWindowFlags_NoScrollbar)) {
 		EventActivate();
 		ApplyTransform();
 
@@ -34,26 +61,159 @@ void NodeEditor::Render(const char* _lable, const ImVec2& _size /*= {0,0}*/)
 		NE_size = ImGui::GetContentRegionAvail() + ImVec2(0, 20);
 		NE_center = pos + NE_size / 2.0f;
 
+
+		ImGui::GetForegroundDrawList()->AddRectFilled(pos, pos + ImGui::GetContentRegionAvail(), IM_COL32(255, 255, 255, 20), 5.0f);
 		ImGui::Text(_lable);
-		ImGui::GetForegroundDrawList()->AddRectFilled(pos, pos + ImGui::GetContentRegionAvail()+ImVec2(0,20), IM_COL32(255, 255, 255, 20), 5.0f);
+		ImGui::SliderFloat("B_curvity", &th_curvity, 0, 15);
+		ImGui::SliderFloat("N_rounding", &th_rounding, 0, 15);
+		ImGui::SliderFloat("P_offset", &th_offset, 2, 10);
+
+
+		ImFont* font = ImGui::GetIO().Fonts->Fonts[1];
+		ImGui::PushFont(font);
+		ImGui::GetStyle().Colors[ImGuiCol_Text] = ImVec4(0.9, 0.9, 0.9, 1);
+
+		///////////////////////// RENDER ///////////////////////////
+		const float rounding = th_rounding * o_scale[0];
+
+		const float pin_offset = th_offset * o_scale[0];
+		const ImVec2 pin_size = ImVec2(0.1, 0.1) * o_scale;
+		const ImVec2 head_size = ImVec2(0, 15) * o_scale;
+
+		const ImVec2 mouse = ImGui::GetMousePos() - ImGui::GetCursorScreenPos();
+
+		const glm::vec2 pos_trans = o_position * o_scale;
 
 		for (auto& node : _node_pool) {
 			//RenderNode(node);
-			
-			const ImVec2 min = NE_center + o_Transform * (node.o_position * glm::vec2(-1, 1) - (ImVec2(50, 40) * node.o_scale / 2.0f));
-			const ImVec2 max = NE_center + o_Transform * (node.o_position * glm::vec2(-1, 1) + (ImVec2(50, 40) * node.o_scale / 2.0f));
-			
+
+			node.min = NE_center + o_Transform * (node->o_position * glm::vec2(-1, 1) - (ImVec2(50, 40) * node->o_scale / 2.0f));
+			node.header = node.min - ImVec2(0, 5) * o_scale;
+			node.max = NE_center + o_Transform * (node->o_position * glm::vec2(-1, 1) + (ImVec2(50, 40) * node->o_scale / 2.0f));
+
+			const ImDrawFlags flags = node->is_open ? ImDrawFlags_RoundCornersTopLeft | ImDrawFlags_RoundCornersTopRight : ImDrawFlags_None;
+			// [  HEAD  ]
 			ImGui::GetWindowDrawList()->AddRectFilled(
-				min,
-				max,
-				IM_COL32(255, 255, 255, 200),
-				5.0f
+				node.header,
+				ImVec2(node.max.x, node.min.y),
+				Nodes::n_color_list[node->n_type],
+				rounding,
+				flags
 			);
+
+			const ImVec2 arror_up = node.min - ImVec2(-2.5, 4.0) * o_scale - ImVec2(8, 0.2);
+			const ImVec2 arror_dn = node.min - ImVec2(-3.0, 4.2) * o_scale - ImVec2(8, 0.2);
+			ImGui::GetWindowDrawList()->AddText(font, 4 * o_scale[0], node.min - ImVec2(-6, 4.5) * o_scale, IM_COL32(255, 255, 255, 255), node->n_name.c_str());
+			ImGui::RenderArrow(ImGui::GetWindowDrawList(), node->is_open ? arror_up : arror_dn, IM_COL32(255, 255, 255, 255), node->is_open ? ImGuiDir_Down : ImGuiDir_Right, o_scale[0] * 0.2);
+
+			if (IsClick())
+				if (node.header < ImGui::GetMousePos() && ImGui::GetMousePos() < node.max) {
+					if (ImGui::GetMousePos() < ImVec2(node.header.x + (node.max.x - node.header.x) * 0.125, node.min.y))
+						node->is_open = !node->is_open;
+					else
+						active_node_id = node->n_id;
+
+					no_node_clicked &= false;
+				}
+
+			//[  Outline  ]
+			if (node->n_id == active_node_id)
+				ImGui::GetWindowDrawList()->AddRect(
+					node.header,
+					node->is_open ? node.max : ImVec2(node.max.x, node.min.y),
+					IM_COL32(255, 255, 255, 255),
+					rounding,
+					0,
+					2.0f
+				);
+
+			bool editing_link = false;
+			// [  BODY  ]
+			if (node->is_open) {
+				ImGui::GetWindowDrawList()->AddRectFilled(
+					node.min,
+					node.max,
+					IM_COL32(10, 10, 10, 255),
+					rounding,
+					ImDrawFlags_RoundCornersBottomLeft | ImDrawFlags_RoundCornersBottomRight
+				);
+
+				ImVec2 inp_curs = node.min + head_size;
+				const float margin = (node.header.y - node.min.y) / 2;
+				for (auto& i : node->n_in) {
+					editing_link |= ImGui::PinButton(i.para_name.c_str(), true, inp_curs, pin_size, false);
+
+					if (Nodes::n_in_link.find(&i) != Nodes::n_in_link.end()) {
+						Nodes::ParaLink& tar_link = Nodes::n_in_link[&i];
+
+						ImVec2 start_p;
+
+						if (tar_link.first->is_open)
+							start_p = _node_pool[_node_index_cache[tar_link.first]].GetOutPinPos(head_size, pin_offset, tar_link.second);
+						else
+							start_p = ImVec2(_node_pool[_node_index_cache[tar_link.first]].max.x, _node_pool[_node_index_cache[tar_link.first]].min.y) + ImVec2(margin, margin);
+						
+						ImGui::GetWindowDrawList()->AddBezierCurve(
+							start_p,
+							start_p + ImVec2(th_curvity * 10, 0) * o_scale,
+							inp_curs - ImVec2(th_curvity * 10, 0) * o_scale,
+							inp_curs,
+							IM_COL32(255, 255, 255, 255),
+							0.5 * o_scale[0]
+						);
+
+					}
+
+					inp_curs.y += pin_offset;
+				}
+
+				ImVec2 outp_curs = ImVec2(node.max.x, node.min.y) + head_size;
+				for (auto& i : node->n_out) {
+					editing_link |= ImGui::PinButton(i.para_name.c_str(), true, outp_curs, pin_size, true);
+					outp_curs.y += pin_offset;
+				}
+			}
+			else {
+				const float margin = (node.header.y - node.min.y) / 2;
+				const ImVec2 pin_pos = node.header - ImVec2(margin, margin);
+				for (auto& i : node->n_in) {
+					if (Nodes::n_in_link.find(&i) != Nodes::n_in_link.end()) {
+						Nodes::ParaLink& tar_link = Nodes::n_in_link[&i];
+
+						ImVec2 end_p;
+						
+						if(tar_link.first->is_open)
+							end_p = _node_pool[_node_index_cache[tar_link.first]].GetOutPinPos(head_size, pin_offset, tar_link.second);
+						else
+							end_p = ImVec2(_node_pool[_node_index_cache[tar_link.first]].max.x, _node_pool[_node_index_cache[tar_link.first]].min.y) + ImVec2(margin, margin);
+
+						//DEBUG(_node_pool[_node_index_cache[tar_link.first]]->n_name)
+						ImGui::GetWindowDrawList()->AddBezierCurve(
+							end_p,
+							end_p + ImVec2(th_curvity * 10, 0) * o_scale,
+							pin_pos - ImVec2(th_curvity * 10, 0) * o_scale,
+							pin_pos,
+							IM_COL32(255, 255, 255, 255),
+							0.5 * o_scale[0]
+						);
+					}
+				}
+			}
+
+			//[  Move  ]
+			if (node->n_id == active_node_id && LMB_press)
+				if (!editing_link)
+					node->Move((glm::vec2(-1, 1) / o_scale) * EventListener::GetDeltaMouse());
+
 		}
 
-
+		ImGui::PopFont();
+		ImGui::GetStyle().Colors[ImGuiCol_Text] = ImVec4(1, 1, 1, 1);
 		ImGui::EndChild();
+
+		Reset();
 	}
+
 }
 
 void NodeEditor::RenderNode(Nodes& _node)
@@ -65,6 +225,23 @@ void NodeEditor::Resize()
 {
 	NE_size = ImGui::GetContentRegionAvail() + ImVec2(0, 20);
 
+}
+
+void NodeEditor::Reset()
+{
+	if (IsClick()) {
+		if (no_node_clicked)
+			active_node_id = 0;
+
+		no_node_clicked = true;
+	}
+
+	LMB_press = false;
+}
+
+void NodeEditor::LMB()
+{
+	LMB_press = true;
 }
 
 void NodeEditor::SHIFT_MMB()
@@ -84,3 +261,5 @@ void NodeEditor::SCRLL()
 	if (ITEM::is_inside(NE_size))
 		Zoom(glm::pow(0.8f, -scroll_dir));
 }
+
+
--- res/HEADS/UI/ITEM/NodeEditor.h
+++ res/HEADS/UI/ITEM/NodeEditor.h
@@ -4,6 +4,7 @@
 #include "Nodes.h"
 
 #include "ImguiItem.h"
+#include "ImGui/imgui_internal.h"
 #include "EventListener.h"
 
 enum NodeEditorType
@@ -11,10 +12,45 @@ enum NodeEditorType
 	NONE_NODE_EDITOR = -1, SHADER_NODE_EDITOR
 };
 
+struct NodeTrans {
+public:
+	NodeTrans(Nodes* _node) :m_node(_node) {}
+
+public:
+	Nodes* m_node;
+
+	ImVec2 min{ 0,0 };
+	ImVec2 max{ 0,0 };
+	ImVec2 header{ 0,0 };
+public:
+	const ImVec2 GetOutPinPos(const ImVec2& _header_size, float _offset, int _idx);
+	const ImVec2 GetInPinPos(const ImVec2& _header_size, float _offset, int _idx);
+
+public:
+	Nodes* operator->() { return m_node; }
+};
+
 class NodeEditor : public EventListener, public Transform2D
 {
 private:
-	std::vector<Nodes> _node_pool;
+	std::vector<NodeTrans> _node_pool;
+	std::unordered_map<Nodes*, int> _node_index_cache;
+	void PushNode(Nodes* _node) { _node_pool.emplace_back(_node); _node_index_cache[_node] = _node_pool.size() - 1; }
+	void PushNodes(const std::vector<Nodes*>& _list) {
+		for (auto& node : _list) { 
+			_node_pool.emplace_back(node);
+			_node_index_cache[node] = _node_pool.size() - 1; 
+		}
+	}
+
+	unsigned int active_node_id;
+private:
+	bool no_node_clicked = true;
+	bool LMB_press = false;
+private:
+	float th_curvity = 1.6f;
+	float th_offset = 5;
+	float th_rounding = 2;
 public:
 	NodeEditorType _type{NONE_NODE_EDITOR};
 	NodeEditor(NodeEditorType type);
@@ -27,7 +63,9 @@ public:
 public:
 	ImVec2 NE_size, NE_center;
 	void Resize();
+	void Reset();
 
+	void LMB();
 	void SHIFT_MMB();
 	void CTRL_MMB();
 	void SCRLL();
