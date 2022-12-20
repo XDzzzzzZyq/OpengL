7fd7cd915e7b1665eddf7500cef1b25e1f3dd311
XdzzzzzzzYQ <81028185+XDzzzzzZyq@users.noreply.github.com>
2022-12-20 20:42:55 +0800

    12.20 DEBUG excluded in release mode

--- res/HEADS/EXTRA/support.h
+++ res/HEADS/EXTRA/support.h
@@ -12,9 +12,19 @@
 #include "xdz_math.h"
 #include "operator.h"
 
+#ifdef _DEBUG
+
 #define DEBUG(x) std::cout<<(x)<<"\n";
-#define DEBUGS(x) std::cout<__func__<<" : "<<__LINE__<<" : "<<(x)<<"\n";<
+#define DEBUGS(x) std::cout<__func__<<" : "<<__LINE__<<" : "<<(x)<<"\n";
 #define GLDEBUG std::cout<<__func__<<" : "<<__LINE__<<" : "<<glDebug_xdz()<<"\n";
+
+#else
+
+#define DEBUG(x) 
+#define DEBUGS(x) 
+#define GLDEBUG 
+
+#endif
 
 //extern "C" {
 //	_declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
