e3ca6f167c7fcb4ea4592275b7f96a0170a6d89b
XDzZyq <xiaodouzizyq@gmail.com>
2024-03-05 10:44:05 +0800

    3.5 mathlib tests

--- test/renderer/test.cpp
+++ test/renderer/test.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include "macros.h"
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -26,4 +27,29 @@ TEST(GLEnvir, Tests) {
 	glGetIntegerv(GL_MAX_FRAMEBUFFER_HEIGHT, &max_resolution_h);
 
 	std::cout << "fb_size = [" << max_resolution_w << ":" << max_resolution_h << "]\n";
+}
+
+
+#include "xdz_math.h"
+#include "xdz_matrix.h"
+TEST(Mathlib, Tests) {
+	EXPECT_EQ(5.0f, xdzm::float_dist(-3, 4));
+	EXPECT_EQ(5.0f, xdzm::float_dist( 3, 4));
+
+	EXPECT_EQ( 1.0f, xdzm::map01_11(1.0f));
+	EXPECT_EQ( 0.0f, xdzm::map01_11(0.5f));
+	EXPECT_EQ(-1.0f, xdzm::map01_11(0.0f));
+
+	EXPECT_EQ(glm::vec3(-1.0f), xdzm::map01_11(glm::vec3(0.0f)));
+	EXPECT_EQ(glm::vec3( 1.0f), xdzm::map01_11(glm::vec3(1.0f)));
+
+	const std::vector<float> v{ 1,2,3 };
+	EXPECT_EQ(glm::vec3(1, 2, 3), xdzm::stdVec3_vec3(v));
+
+	const auto rand = xdzm::rand3nhv(10);
+	EXPECT_EQ(rand.size(), 10);
+	LOOP(10)
+		EXPECT_TRUE(1.0f - glm::length(rand[i]) < glm::epsilon<float>());
+
+	EXPECT_EQ(4.0f, xdzm::_matrix_sum<4>(xdzm::identityMatrix));
 }
