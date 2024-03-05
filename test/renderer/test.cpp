ca66f1c5bd7ad39de5e1da9808428e3cef61cc74
XDzZyq <xiaodouzizyq@gmail.com>
2024-03-05 09:59:16 +0800

    Test: OpenGL 3.1 environment (#10)
    * 2.27 Google Test
    
    * 2.27 GLFW & GLEW environment
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * 2.27 dependencies fixes
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * 2.27 test dlls
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * 3.4 remove version spec
    
    * 3.5 configuring
    
    * 3.5 higher openGL version
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Update msbuild.yml
    
    * Create opengl.yml (#9)
    
    * Update msbuild.yml
    
    * Update opengl.yml
    
    * Delete .github/workflows/opengl.yml
    
    * Update msbuild.yml

---
+++ test/renderer/test.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+TEST(GTestBasic, Tests) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+TEST(GLEnvir, Tests) {
+	EXPECT_TRUE(glfwInit()) << "glfw init error" << std::endl;
+
+	GLFWwindow* window = glfwCreateWindow(100, 100, "TEST_WINDOW", NULL, NULL);
+	EXPECT_TRUE(window) << "window error" << std::endl;
+	glfwMakeContextCurrent(window);
+
+	EXPECT_EQ(glewInit(), GLEW_OK) << "glew error" << std::endl;
+	EXPECT_EQ(glGetError(), GL_NO_ERROR) << "OpenGL Error: " << glGetError() << std::endl;
+
+	const GLubyte* version = glGetString(GL_VERSION);
+	std::cout << version << "\n";
+
+	GLint max_resolution_w, max_resolution_h;
+	glGetIntegerv(GL_MAX_FRAMEBUFFER_WIDTH, &max_resolution_w);
+	glGetIntegerv(GL_MAX_FRAMEBUFFER_HEIGHT, &max_resolution_h);
+
+	std::cout << "fb_size = [" << max_resolution_w << ":" << max_resolution_h << "]\n";
+}
